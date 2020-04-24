f0acde81259f05ca3eb049d73bb8c30d61ed1121
AlexKoloCorp <kolomiets.alex.home@gmail.com>
2020-04-24 11:18:37 +0300

    simple login activity

--- volks/MainActivity.cs
+++ volks/MainActivity.cs
@@ -3,17 +3,43 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using System;
+using System.Threading.Tasks;
 
 namespace volks
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        EditText username, password;      
+        Button button;
         protected override void OnCreate(Bundle savedInstanceState)
-        {
+        {      
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+             username = FindViewById<EditText>(Resource.Id.Username_zone);
+             password = FindViewById<EditText>(Resource.Id.Password_zone);
+             button = FindViewById<Button>(Resource.Id.Login_button);
+            button.Click += Button_Click;
+
         }
+
+        private void Button_Click(object sender, System.EventArgs e)
+        {
+            //string usernameText=username.Text.ToString();
+            //string pswd = password.Text.ToString();
+            User user = new User(username.Text.ToString(), password.Text.ToString());
+            if (user.CheckInfo())
+            {
+                Toast.MakeText(this,"logined",ToastLength.Short).Show();
+            }
+            else {
+                Toast.MakeText(this, "you didn't fill all fields", ToastLength.Short).Show();
+            }
+            //throw new System.NotImplementedException();
+        }
+
+      
     }
 }
---
+++ volks/model/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace volks
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public User() { }
+        public  User(string Username, string Password) {
+            this.Username = Username;
+            this.Password = Password;
+        }
+        public bool CheckInfo() {
+            if (!this.Username.Equals("") && !this.Password.Equals(""))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
