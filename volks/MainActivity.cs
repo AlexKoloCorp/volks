cb817692a6896cb207cdc98635372322ee5242e6
AlexKoloCorp <kolomiets.alex.home@gmail.com>
2020-05-09 16:32:39 +0300

    add user cabinet, and add some style

--- volks/MainActivity.cs
+++ volks/MainActivity.cs
@@ -5,6 +5,8 @@ using Android.Runtime;
 using Android.Widget;
 using System;
 using System.Threading.Tasks;
+using Android.Content;
+using volks.volksActivities;
 
 namespace volks
 {
@@ -12,7 +14,7 @@ namespace volks
     public class MainActivity : AppCompatActivity
     {
         EditText username, password;      
-        Button button;
+        Button btn_logIn,btn_go_to_carLog;
         protected override void OnCreate(Bundle savedInstanceState)
         {      
             base.OnCreate(savedInstanceState);
@@ -20,9 +22,17 @@ namespace volks
             SetContentView(Resource.Layout.activity_main);
              username = FindViewById<EditText>(Resource.Id.Username_zone);
              password = FindViewById<EditText>(Resource.Id.Password_zone);
-             button = FindViewById<Button>(Resource.Id.Login_button);
-            button.Click += Button_Click;
+            btn_logIn = FindViewById<Button>(Resource.Id.Login_button);
+            btn_logIn.Click += Button_Click;
+            btn_go_to_carLog = FindViewById<Button>(Resource.Id.Sigin_button);
+            btn_go_to_carLog.Click += Btn_go_to_carLog_Click;
+        }
 
+        private void Btn_go_to_carLog_Click(object sender, EventArgs e)
+        {
+            Intent NextActivity = new Intent(this, typeof(UserCarLog));
+            StartActivity(NextActivity);
+            //throw new NotImplementedException();
         }
 
         private void Button_Click(object sender, System.EventArgs e)
@@ -38,8 +48,6 @@ namespace volks
                 Toast.MakeText(this, "you didn't fill all fields", ToastLength.Short).Show();
             }
             //throw new System.NotImplementedException();
-        }
-
-      
+        }    
     }
 }
---
+++ volks/volksActivities/UserCarLog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace volks.volksActivities
+{
+    [Activity(Label = "UserCarLog")]
+    public class UserCarLog : Activity
+    {
+        List<string> list;           
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.user_carLog);
+            var listView = FindViewById<ListView>(Resource.Id.Car_Info_List);
+            list = new List<string>();
+            list.AddRange(new string[] { "Color", "Date of purchase", "Warranty period" });
+            ArrayAdapter<string> adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, list);
+            listView.Adapter = adapter;
+        }
+    }
+}
